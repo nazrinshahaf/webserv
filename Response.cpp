e8a3b6ad928dad160b89dde13b3f762dfe2b48bb
Nazrin Shah Adrian Fernandez <nazrin2222@gmail.com>
2023-03-08 23:25:29 +0800

    created auto index template

--- Response.cpp
+++ Response.cpp
@@ -1,6 +1,22 @@
 #include "Response.hpp"
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <sys/dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define _XOPEN_SOURCE 700 //for autoindex
+#define _GNU_SOURCE //for checking audoindex file type
 
 using namespace webserv;
+
+using std::cout;
+using std::endl;
 
 Response::Response()
 {
@@ -17,7 +33,7 @@ Response::Response(const Request &req, const string &root_path, std::map<int, st
 
 void Response::respond(void)
 {
-    const char *header = "HTTP/1.1 200 OK\nContent-Type: text/html\n\n"; // Dynamically add content length TODO
+    const char *header = "HTTP/1.1 200 OK\nContent-Type: */*\n\n"; // Dynamically add content length TODO
     const char *header2 = "HTTP/1.1 200 OK\nContent-Type: image/*\r\n\r\n";
     const char *header_404 = "HTTP/1.1 404 Not Found\nContent-Type: text/html\n\n";
 
@@ -25,10 +41,18 @@ void Response::respond(void)
     string entireText;
     string line;
 
-    if (_req.path() == "/")
-        myfile.open(_root_path + "/index.html", std::ios::binary);
-    else
-        myfile.open(_root_path + _req.path(), std::ios::binary);
+	if (_req.path() == "/")
+	{
+		myfile.open(_root_path + "/index.html", std::ios::binary);
+	}
+	else if (_req.path() == "/autoindex")
+	{
+		entireText += header;
+		entireText += handle_auto_index(_root_path);
+	}
+	else
+		myfile.open(_root_path + _req.path(), std::ios::binary);
+
     if (!myfile)
     {
         entireText += header_404;
@@ -69,4 +93,139 @@ void Response::respond(void)
             cout << "total sent :" << total_sent << endl;
         }
     }
+}
+
+/*
+ * change this later pls..
+ * @note : DONT LOOK AT THIS PLS
+ * */
+
+static string	auto_index_apply_syle(void)
+{
+	//return (" html { min-height: 100%; text-align: center; display: flex; justify-content: center; flex-direction: column; } body { font-family: Arial, sans-serif; margin: 0; padding: 0; text-align: center; min-height: 100%; } h1 { text-align: center; } table { border: none; border-bottom: 1px solid black; border-collapse: collapse; margin: 0 auto; width: 600px; } thead { border-bottom: 1px solid black; } th, td { /* border: 1px solid black; */ padding: 10px; text-align: left; } td a { font-weight: 600; }");
+	return("<link rel=\"stylesheet\" href=\"autoindex.css\">");
+}
+
+static string		auto_index_create_header(const string &path)
+{
+	string header;
+	header += "<div class=\"header\">";
+	header += "<div class=\"box\">";
+	header += "<h1 class=\"box-text-1\">";
+	header += "AutoIndex of";
+	header += "</h1>";
+	header += "<p class=\"box-text-2\">";
+	header += path;
+	header += "</p>";
+	header += "</div>";
+	header += "</div>";
+	return (header);
+}
+
+static string		auto_index_create_html(const int &type, const string &path = string())
+{
+	string	header;
+
+	if (type == 0) //header
+	{
+		header += "<!DOCTYPE html>";
+		header += "<html>";
+		header += "<head>";
+		/* header += "<style>" + auto_index_apply_syle() + "</style>"; */
+		header += auto_index_apply_syle();
+		header += "</head>";
+		header += "<body>";
+		header += auto_index_create_header(path);
+		header += "<table>";
+		header += "<thead>";
+		header += "<tr>";
+		header += "<th>Name</th>";
+		header += "<th>Last Modified</th>";
+		header += "<th>Size</th>";
+		header += "</tr>";
+		header += "</thead>";
+		header += "<tbody>";
+	}
+	else if (type == 1) //footer
+	{
+		header += "</tbody>";
+		header += "</table>";
+		header += "<body>";
+		header += "<html>";
+	}
+	return (header);
+}
+
+static string			auto_index_print_last_modified(const string &path, const string &file_name)
+{
+	struct stat attr;
+	string		full_path = path + "/" + file_name;
+
+	stat(full_path.c_str(), &attr);
+	return (string(ctime(&attr.st_mtime)));
+}
+
+static string			auto_index_print_file_size(const string &path, const string &file_name)
+{
+	struct stat attr;
+	string		full_path = path + "/" + file_name;
+
+	stat(full_path.c_str(), &attr);
+	if (attr.st_size > 1000)
+		return (std::to_string(attr.st_size/1000) + "MB");
+	return (std::to_string(attr.st_size) + "B");
+}
+
+static string			auto_index_create_file(const string &path, const int &file_type, const string &file_name)
+{
+	string	file_str;
+	string	full_path = path + "/" + file_name;
+
+	cout << file_name << endl;
+	if (file_type == 1) //everything else (mainly file)
+	{
+		file_str += "<tr>";
+		file_str += "<td><a href=\"" + full_path + "\">" + file_name + "</a></td>";
+		file_str += "<td>" + auto_index_print_last_modified(path, file_name) + "</td>";
+		file_str += "<td>" + auto_index_print_file_size(path, file_name) + "</td>";
+		file_str += "</tr>";
+	}
+	else if (file_type == 0) //directory
+	{
+		string	file_name_with_slash = (file_name == "." || file_name == "..") ? file_name : file_name + "/";
+		file_str += "<tr>";
+		file_str += "<td><a href=\"" + full_path + "\">" + file_name_with_slash + "</a></td>";
+		file_str += "<td>" + auto_index_print_last_modified(path, file_name) + "</td>";
+		file_str += "<td>-</td>";
+		file_str += "</tr>";
+	}
+	return (file_str);
+}
+
+std::string	Response::handle_auto_index(const string &path)
+{
+	string						auto_index_html;
+	DIR							*directory;
+	struct dirent				*file;     
+	std::multimap<int, string>	sorted_mmap;
+
+	auto_index_html += auto_index_create_html(0, path);
+	directory = opendir(path.c_str());
+	if (directory != NULL)
+	{
+		while((file = readdir(directory)) != NULL)
+		{
+			int	file_type = (file->d_type == DT_REG) ? 1 : 0;
+			sorted_mmap.insert(std::make_pair(file_type, string(file->d_name)));
+		}
+		(void)closedir(directory);
+	}
+	else
+		perror("Couldn't open the directory");
+	for (std::multimap<int,string>::iterator it = sorted_mmap.begin(); it != sorted_mmap.end(); it++) {
+		auto_index_html += auto_index_create_file(path, it->first, it->second);
+	}
+	auto_index_html += auto_index_create_html(1);
+	cout << auto_index_html << endl;
+	return (auto_index_html);
 }
--- Response.hpp
+++ Response.hpp
@@ -7,10 +7,11 @@
 #include <fstream>
 #include <map>
 #include <sys/socket.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 using std::string;
-using std::cout;
-using std::endl;
 
 namespace webserv
 {
@@ -21,11 +22,13 @@ namespace webserv
         string _root_path;
         std::map<int, string>::iterator _it;
 
+		string	handle_auto_index(const string &path);
+
     public:
         Response();
         Response(const Request &req, const string &root_path, std::map<int, string>::iterator &it);
 
-        void respond(void);
+        void			respond(void);
     };
 }
 
